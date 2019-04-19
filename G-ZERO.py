8b9c8fa30822d67f6cc00ee474a4e260692db1a2
PunyaweeSrithongkerd <48820372+PunyaweeSrithongkerd@users.noreply.github.com>
2019-04-19 13:41:01 +0700

    Add files via upload
    3RD submission:Interface,Enemy update, and Beam

--- G-ZERO.py
+++ G-ZERO.py
@@ -3,6 +3,7 @@ from models import World
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+BLOCK_SIZE = 40
 
 
 class ModelSprite(arcade.Sprite):
@@ -19,26 +20,45 @@ class ModelSprite(arcade.Sprite):
         self.sync_with_model()
         super().draw()
 
-class Wall:
-    def __init__(self):
-        self.sprite = arcade.Sprite('images\wall.png')
-    def draw(self,wall_list):
-        for wall in wall_list:
-            self.sprite.set_position(wall[0],wall[1])
-            self.sprite.draw()
+
+class MazeDrawer():
+    def __init__(self, maze):
+        self.maze = maze
+        self.width = self.maze.width
+        self.height = self.maze.height
+
+        self.wall_sprite = arcade.Sprite('images/wall.png')
+
+    def get_sprite_position(self, r, c):
+        x = c * BLOCK_SIZE + (BLOCK_SIZE // 2)
+        y = r * BLOCK_SIZE + (BLOCK_SIZE + (BLOCK_SIZE // 2))
+        return x,y
+
+    def draw_sprite(self, sprite, r, c):
+        x, y = self.get_sprite_position(r, c)
+        sprite.set_position(x, y)
+        sprite.draw()
+
+    def draw(self):
+        for r in range(self.height):
+            for c in range(self.width):
+                if self.maze.has_wall_at(r, c):
+                    self.draw_sprite(self.wall_sprite, r, c)
+
+
 class GameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         arcade.set_background_color(arcade.color.BLACK)
-        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT, BLOCK_SIZE)
         self.hinu_sprite = ModelSprite('images/hinugundam.png',
                                        model=self.world.gundam)
         self.enemy_sprite = ModelSprite('images/enemy.png',
                                         model=self.world.enemy)
         self.hinu_sprite.set_position(300, 300)
         self.enemy_sprite.set_position(600, 600)
-        self.wall = Wall()
+        self.wall = MazeDrawer(self.world.interface)
 
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
@@ -48,10 +68,12 @@ class GameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.hinu_sprite.draw()
+        self.world.beam_list.draw()
         self.enemy_sprite.draw()
-        self.wall.draw(self.world.interface.wall)
-        
+        self.wall.draw()
+        self.hinu_sprite.draw()
+
+
 
 def main():
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 import arcade.key
 
-
 MOVEMENT_SPEED = 4
-BEAM_SPEED = 5
 DIR_STILL = 0
 DIR_UP = 1
 DIR_RIGHT = 2
@@ -15,72 +13,128 @@ DIR_OFFSETS = {DIR_STILL: (0, 0),
                DIR_DOWN: (0, -1),
                DIR_LEFT: (-1, 0)}
 
-class Interface:
-    def __init__(self,world):
-        self.map =  [ '####################',
-                     '#..................#',
-                     '#.###.###..###.###.#',
-                     '#.#...#......#...#.#',
-                     '#.#.###.####.###.#.#',
-                     '#.#.#..........#.#.#',
-                     '#.....###..###.....#',
-                     '#.#.#..........#.#.#',
-                     '#.#.###.####.###.#.#',
-                     '#.#...#......#...#.#',
-                     '#.###.###..###.###.#',
-                     '#..................#',
-                     '####################' ]
-        self.height = len(self.map)
-        self.width = len(self.map[0])
-        self.wall = []
-        for y in range(len(self.map)):
-            line = self.map[y]
-            for x in range(len(line)):
-                char = line[x]
-                if char == '#':
-                    self.wall.append([(x*40)+20,(y*40)+20])
+KEY_MAP = {arcade.key.UP: DIR_UP,
+           arcade.key.DOWN: DIR_DOWN,
+           arcade.key.LEFT: DIR_LEFT,
+           arcade.key.RIGHT: DIR_RIGHT}
+
+BLOCK_SIZE = 40
+BLOCK_CENTER = 20
+
 
 class Gundam:
-    def __init__(self, world, x, y):
+    def __init__(self, world, x, y, interface, block_size):
         self.world = world
         self.x = x
         self.y = y
         self.direction = DIR_STILL
-        self.gundam_center_x = 38
+        self.interface = interface
+        self.block_size = block_size
+        self.next_direction = DIR_STILL
+
     def move(self, direction):
         self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
         self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
 
+    def is_at_center(self):
+        half_size = self.block_size // 2
+        return (((self.x - half_size) % self.block_size == 0) and
+                ((self.y - half_size) % self.block_size == 0))
+
+    def get_row(self):
+        return (self.y - self.block_size) // self.block_size
+
+    def get_col(self):
+        return self.x // self.block_size
+
+    def check_walls(self, direction):
+        new_r = self.get_row() + DIR_OFFSETS[direction][1]
+        new_c = self.get_col() + DIR_OFFSETS[direction][0]
+        return not self.interface.has_wall_at(new_r, new_c)
+
     def update(self, delta):
+        # print(self.direction, self.x, self.y)
+        if self.is_at_center():
+            if self.check_walls(self.next_direction):
+                self.direction = self.next_direction
+            else:
+                self.direction = DIR_STILL
+
         self.move(self.direction)
 
 
 class Enemy:
+    ENEMY_SPEED = 5
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
 
+    def update(self, delta):
+        self.x -= Enemy.ENEMY_SPEED
+        if self.x < 0:
+            self.x = self.world.width
+
+
+class Interface:
+    def __init__(self,world):
+        self.map = ['####################',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '####################',
+                    '#..................#',
+                    '#..................#',
+                    '#..................#',
+                    '####################']
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.map.reverse()
+
+    def has_wall_at(self, r, c):
+        return self.map[r][c] == '#'
+
+    def has_dot_at(self, r, c):
+        return self.map[r][c] == '.'
+
 
 class World:
-    def __init__(self, width, height):
+    def __init__(self, width, height, block_size):
         self.width = width
         self.height = height
-        self.gundam = Gundam(self, width // 8, height // 2)
-        self.enemy = Enemy(self, width -200, height // 2)
         self.interface = Interface(self)
+        self.block_size = block_size
+        self.gundam = Gundam(self, width // 8, height // 2,
+                             self.interface, self.block_size)
+        self.enemy = Enemy(self, width - 200, height // 2)
+        self.beam_list = arcade.SpriteList()
 
     def on_key_press(self, key, key_modifiers):
-        if key == arcade.key.UP:
-            self.gundam.direction = DIR_UP
-        if key == arcade.key.DOWN:
-            self.gundam.direction = DIR_DOWN
-        if key == arcade.key.LEFT:
-            self.gundam.direction = DIR_LEFT
-        if key == arcade.key.RIGHT:
-            self.gundam.direction = DIR_RIGHT
-
-
+        if key in KEY_MAP:
+            self.gundam.next_direction = KEY_MAP[key]
+        if key == arcade.key.SPACE:
+            beam = arcade.Sprite("images/Beam shot.png")
+            beam_speed = 5
+            beam.angle = 180
+            beam.change_x = beam_speed
+            beam.set_position(self.gundam.x + 92,
+                              self.gundam.y)
+            self.beam_list.append(beam)
 
     def update(self, delta):
+        screen_width = 800
+        self.beam_list.update()
+        for beam in self.beam_list:
+            if beam.left > screen_width:
+                beam.kill()
         self.gundam.update(delta)
+        self.enemy.update(delta)
+
+
+
